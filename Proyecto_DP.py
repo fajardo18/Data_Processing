266de7c239c4bddefe03278417954d818f0f5401
Angelica Fajardo <fajardo1806@gmail.com>
2023-11-07 23:10:36 -0500

    Parte 4: Procesando información en bruto

--- Proyecto_DP.py
+++ Proyecto_DP.py
@@ -50,3 +50,23 @@ cantidad_mujeres_fumadoras = len(mujeres_fumadoras)
 # Imprimir los resultados
 print(f"Cantidad de hombres fumadores: {cantidad_hombres_fumadores}")
 print(f"Cantidad de mujeres fumadoras: {cantidad_mujeres_fumadoras}")
+
+# Parte 4.
+
+import requests
+
+
+def descargar_archivo_csv_desde_url(url, nombre_archivo):
+    respuesta = requests.get(url)
+
+    if respuesta.status_code == requests.codes.ok:
+        with open(nombre_archivo, 'wb') as archivo:
+            archivo.write(respuesta.content)
+        print(f"Archivo descargado y guardado como {nombre_archivo}")
+    else:
+        print(f"Error al descargar el archivo. Código de respuesta: {respuesta.status_code}")
+
+
+url = "https://huggingface.co/datasets/mstz/heart_failure/raw/main/heart_failure_clinical_records_dataset.csv"
+nombre_archivo = "heart_failure_dataset.csv"
+descargar_archivo_csv_desde_url(url, nombre_archivo)
